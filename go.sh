63eab427f525e03eae2cbad0852690e76a3d624c
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-29 11:15:48 -0800

    Update for 1.1-R2

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-CLASSPATH=../craftbukkit-1.1-R1.jar javac *.java -Xlint:deprecation -Xlint:unchecked
+CLASSPATH=../craftbukkit-1.1-R2.jar javac *.java -Xlint:deprecation -Xlint:unchecked
 rm -rf me
 mkdir -p me/exphc/EnchantMore
 mv *.class me/exphc/EnchantMore/
